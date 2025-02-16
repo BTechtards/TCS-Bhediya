837ec55aa2b585f2bf217a24a4ad0685c6cfea7f
rv178 <idliyout@gmail.com>
2025-02-17 04:44:16 +0530

    [feat] use GatewayIntentBits and "fix" null checks + member count fix

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -4,14 +4,20 @@ import log from "../utils/logger";
 import { ActivityType } from "discord.js";
 
 export default new Event("ready", () => {
-	log.warn(`Logged in as ${client.user.tag}.`);
+	log.warn(`Logged in as ${client.user?.tag}.`);
+
+	let memberCount = 0;
+	client.guilds.cache.forEach(guild => {
+		console.log(guild.memberCount);
+		memberCount += guild.memberCount;
+	});
+
 	setInterval(async () => {
 		const textList = [
-			"you",
-			`${client.users.cache.size} btechtards`,
-			"mommy asmr",
+			`${memberCount} btechtards`,
+			"mommy asmr videos", // very important
 		];
 		const text = textList[Math.floor(Math.random() * textList.length)];
-		client.user.setActivity(text, { type: ActivityType.Watching });
+		client.user?.setActivity(text, { type: ActivityType.Watching });
 	}, 30000); // milliseconds
 });
