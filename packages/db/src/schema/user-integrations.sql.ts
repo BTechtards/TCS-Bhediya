2fbed063c2e6381eba89ad4439eb19adde3846bf
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2025-02-18 00:28:54 +0530

    feat: db package with drizzle set up

---
+++ packages/db/src/schema/user-integrations.sql.ts
@@ -0,0 +1,13 @@
+import { pgTable, timestamp, uuid, pgEnum, text } from 'drizzle-orm/pg-core';
+
+export const userIntegrationType = pgEnum('user_integration_type', [
+    'codeforces',
+    'github',
+]);
+
+export const userIntegrations = pgTable('user_integrations', {
+    id: uuid('id').defaultRandom().primaryKey(),
+    userId: text('user_id').notNull(),
+    type: userIntegrationType('type').notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+});
