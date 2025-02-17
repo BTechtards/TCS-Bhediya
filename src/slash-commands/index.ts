a7e1cb02f6e4bdcba481c938931ca3292531c7f1
aryannn053 <aryannn053@gmail.com>
2025-02-17 20:10:12 +0530

    [feat] github and codeforces stats

--- src/slash-commands/index.ts
+++ src/slash-commands/index.ts
@@ -1,5 +1,10 @@
 import type { SlashCommand } from '@/types/command';
 import ping from './info/ping';
 import codeforces from './info/codeforces'
+import cfprofile from './info/cfprofile';
+import unlink from './info/unlink';
+import linkgithub from './info/linkgithub';
+import unlinkgithub from './info/unlinkgithub';
+import github from './info/github';
 
-export default [ping, codeforces] satisfies SlashCommand[];
+export default [ping, codeforces, cfprofile, unlink, linkgithub, unlinkgithub, github] satisfies SlashCommand[];
---
+++ src/slash-commands/info/unlink.ts
@@ -0,0 +1,34 @@
+import { SlashCommandBuilder } from "discord.js";
+import fs from "fs";
+import { logger } from "@/utils/logger";
+
+function unlinkUsername(discordId) {
+    try {
+        const data = fs.readFileSync("./links.json", "utf-8");
+        const links = JSON.parse(data);
+
+        if (!links[discordId]) return false;
+        
+        delete links[discordId];
+        fs.writeFileSync("./links.json", JSON.stringify(links, null, 2));
+        return true;
+    } catch (err) {
+        logger.error(`Error updating links.json: ${err}`);
+        return false;
+    }
+}
+
+export default {
+    builder: new SlashCommandBuilder()
+        .setName("unlink")
+        .setDescription("Unlink your Codeforces account from your Discord account."),
+    chatCommandHandler: async (interaction) => {
+        const discordId = interaction.user.id;
+        
+        if (unlinkUsername(discordId)) {
+            await interaction.reply({ content: "Your Codeforces account has been unlinked successfully.", ephemeral: true });
+        } else {
+            await interaction.reply({ content: "You don’t have a linked Codeforces account.", ephemeral: true });
+        }
+    },
+};
