c4aee7e9594955af93093c3aca6b9948820bed87
Zeeman <zee@man.com>
2025-02-17 22:01:11 +0530

    feat: biome + lint pass

--- src/structures/client.ts
+++ src/structures/client.ts
@@ -1,90 +1,101 @@
-import { ApplicationCommandDataResolvable, Client, ClientOptions, Collection } from "discord.js";
-import { MessageContextMenuCommand, SlashCommand, UserContextMenuCommand } from "@/types/command";
-// import { sendHook } from "@/utils/functions";
-import { logger } from "@/utils/logger";
-import { Env } from '@/utils/readEnv';
-import slashCommands from '@/slash-commands';
 import events from '@/events';
-import { type EventDependencies } from './event';
+import slashCommands from '@/slash-commands';
+import type {
+    MessageContextMenuCommand,
+    SlashCommand,
+    UserContextMenuCommand,
+} from '@/types/command';
+// import { sendHook } from "@/utils/functions";
+import { logger } from '@/utils/logger';
+import type { Env } from '@/utils/readEnv';
+import {
+    type ApplicationCommandDataResolvable,
+    Client,
+    type ClientOptions,
+    Collection,
+} from 'discord.js';
+import type { EventDependencies } from './event';
 
 export type BotConfig = {
-	env: Env,
-}
+    env: Env;
+};
 
 export class Bot extends Client {
-	slashCommands: Collection<string, SlashCommand> = new Collection();
-	userContextMenuCommands: Collection<string, UserContextMenuCommand> = new Collection();
-	messageContextMenuCommands: Collection<string, MessageContextMenuCommand> = new Collection();
+    slashCommands: Collection<string, SlashCommand> = new Collection();
+    userContextMenuCommands: Collection<string, UserContextMenuCommand> =
+        new Collection();
+    messageContextMenuCommands: Collection<string, MessageContextMenuCommand> =
+        new Collection();
 
-	constructor(public config: BotConfig, clientOptions: ClientOptions) {
-		super(clientOptions);
-	}
+    constructor(
+        public config: BotConfig,
+        clientOptions: ClientOptions,
+    ) {
+        super(clientOptions);
+    }
 
-	async start() {
-		await this.registerCommands();
-		await this.registerEvents();
-		this.login(this.config.env.DISCORD_TOKEN);
-	}
+    async start() {
+        await this.registerCommands();
+        await this.registerEvents();
+        this.login(this.config.env.DISCORD_TOKEN);
+    }
 
-	setup_error_hook() {
-		// ! Bad practice
-		// let error_hook: string;
+    setup_error_hook() {
+        // ! Bad practice
+        // let error_hook: string;
+        // if (process.env.ERROR_LOG) {
+        // 	error_hook = process.env.ERROR_LOG;
+        // } else {
+        // 	log.error("error log webhook url not given!");
+        // 	process.exit();
+        // }
+        // process.on("uncaughtException", (err) => {
+        // 	log.error(err.stack);
+        // 	sendHook(
+        // 		error_hook,
+        // 		"uncaughtException",
+        // 		"```" + err.stack + "```",
+        // 		this.user!.username,
+        // 		this.user!.displayAvatarURL(),
+        // 	);
+        // });
+    }
 
-		// if (process.env.ERROR_LOG) {
-		// 	error_hook = process.env.ERROR_LOG;
-		// } else {
-		// 	log.error("error log webhook url not given!");
-		// 	process.exit();
-		// }
+    async registerCommands() {
+        const slashCommandsData: ApplicationCommandDataResolvable[] = [];
 
-		// process.on("uncaughtException", (err) => {
-		// 	log.error(err.stack);
-		// 	sendHook(
-		// 		error_hook,
-		// 		"uncaughtException",
-		// 		"```" + err.stack + "```",
-		// 		this.user!.username,
-		// 		this.user!.displayAvatarURL(),
-		// 	);
-		// });
-	}
+        slashCommands.forEach((command) => {
+            logger.info(`Loaded command ${command.builder.name} ✅`);
+            this.slashCommands.set(command.builder.name, command);
+            slashCommandsData.push(command.builder.toJSON());
+        });
 
-	async registerCommands() {
-		const slashCommandsData: ApplicationCommandDataResolvable[] = [];
+        this.once('ready', async () => {
+            const guildId = this.config.env.DEV_GUILD_ID;
 
-		slashCommands.forEach(command => {
-			logger.info(`Loaded command ${command.builder.name} ✅`);
-			this.slashCommands.set(command.builder.name, command);
-			slashCommandsData.push(command.builder.toJSON());
-		});
+            logger.info('Loading application (/) commands.');
 
-		this.once("ready", async () => {
-			const guildId = this.config.env.DEV_GUILD_ID; 
+            // ? Dont you need to register the commands with discord
+            if (guildId) {
+                logger.info(`Setting slash commands in ${guildId}`);
+                this.guilds.cache.get(guildId)?.commands.set(slashCommandsData);
+            } else {
+                logger.error('Put a valid DEV_GUILD_ID in .env');
+                process.exit();
+            }
 
-			logger.info("Loading application (/) commands.");
+            logger.info('Finished loading application (/) commands.');
+            this.setup_error_hook();
+        });
+    }
 
-			// ? Dont you need to register the commands with discord
-			if (guildId) {
-				logger.info(`Setting slash commands in ${guildId}`);
-				this.guilds.cache.get(guildId)?.commands.set(slashCommandsData);
-			} else {
-				logger.error("Put a valid DEV_GUILD_ID in .env");
-				process.exit();
-			}
-
-			logger.info("Finished loading application (/) commands.");
-			this.setup_error_hook();
-		});
-	}
-
-	async registerEvents() {
-		const evtDeps: EventDependencies = {
-			client: this,
-		};
-		events.forEach(evt => {
-			// @ts-expect-error TS is dumb and cant prove that this is correct
-			this.on(evt.name, evt.handler(evtDeps));
-		});
-	}
+    async registerEvents() {
+        const evtDeps: EventDependencies = {
+            client: this,
+        };
+        events.forEach((evt) => {
+            // @ts-expect-error TS is dumb and cant prove that this is correct
+            this.on(evt.name, evt.handler(evtDeps));
+        });
+    }
 }
-
