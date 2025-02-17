2965bfb406b0ea37c08fe15420edf6998d5b65ef
rv178 <idliyout@gmail.com>
2025-02-17 16:43:12 +0530

    [feat] status shows subreddit member count

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,19 +1,30 @@
 import { ActivityType } from "discord.js";
 import { Event } from "@/structures/event";
 import { logger } from "@/utils/logger";
+import { getRedditStats } from "@/utils/functions";
 
-export default new Event("ready", (d) => (client) => {
+export default new Event("ready", (d) => async (client) => {
 	logger.info(`Logged in as ${client.user?.tag}`);
 
 	let memberCount = 0;
+	let redditCount = 0;
+
+	try {
+		const uc = await getRedditStats();
+		redditCount += uc;
+	} catch (err) {
+		logger.error(`Error: ${err}`);
+	}
+
+
 	client.guilds.cache.forEach(guild => {
-		console.log(guild.memberCount);
 		memberCount += guild.memberCount;
 	});
 
 	setInterval(async () => {
 		const textList = [
-			`${memberCount} btechtards`,
+			`${memberCount} discord members`,
+			`${redditCount} btechtards on reddit`,
 			"mommy asmr videos", // very important
 		];
 		const text = textList[Math.floor(Math.random() * textList.length)];
