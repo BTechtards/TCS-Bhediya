51af1e6a4fbe37d5dd1ca2c4d7dc9a384caa9679
aryannn053 <aryannn053@gmail.com>
2025-02-18 13:39:58 +0530

    fix

--- src/slash-commands/index.ts
+++
@@ -1,9 +0,0 @@
-import type { SlashCommand } from '@/types/command';
-import ping from './info/ping';
-import codeforces from './info/codeforces'
-import cfprofile from './info/cfprofile';
-import unlink from './info/unlink';
-import unlinkgithub from './info/unlinkgithub';
-import github from './info/github';
-
-export default [ping, codeforces, cfprofile, unlink, unlinkgithub, github] satisfies SlashCommand[];
