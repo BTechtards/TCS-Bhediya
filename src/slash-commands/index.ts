7f85d46614604a2c1b68ee1f2b52fe356af244fa
aryannn053 <aryannn053@gmail.com>
2025-02-18 13:38:09 +0530

    [update] slash commands

--- src/slash-commands/index.ts
+++ src/slash-commands/index.ts
@@ -3,8 +3,7 @@ import ping from './info/ping';
 import codeforces from './info/codeforces'
 import cfprofile from './info/cfprofile';
 import unlink from './info/unlink';
-import linkgithub from './info/linkgithub';
 import unlinkgithub from './info/unlinkgithub';
 import github from './info/github';
 
-export default [ping, codeforces, cfprofile, unlink, linkgithub, unlinkgithub, github] satisfies SlashCommand[];
+export default [ping, codeforces, cfprofile, unlink, unlinkgithub, github] satisfies SlashCommand[];
