f0de9daeffd504c7d03c7aced074856204020984
aryannn053 <aryannn053@gmail.com>
2025-02-19 20:47:50 +0530

    [feat] codeforces command

--- packages/db/migrations/0000_brainy_epoch.sql
+++ packages/db/migrations/0000_brainy_epoch.sql
@@ -3,5 +3,4 @@ CREATE TABLE "user_integrations" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"user_id" text NOT NULL,
 	"type" "user_integration_type" NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL
 );
